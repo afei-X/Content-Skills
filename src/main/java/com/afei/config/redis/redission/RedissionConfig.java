ee6c1fb008101998ae1ad65470bf7e2e33468e09
fly the sky <fX#H#3Vs>
2021-04-13 10:09:22 +0800

    commit

---
+++ src/main/java/com/afei/config/redis/redission/RedissionConfig.java
@@ -0,0 +1,17 @@
+package com.afei.config.redis.redission;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissionConfig {
+
+
+    public static void main(String[] args) {
+        Integer integer = new Integer(25);
+        System.out.println(25==integer);
+    }
+
+
+
+}
